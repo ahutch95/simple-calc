06086790a9f9ecc82c120c3b15218efa7831f520
Austin Hutchinson <ahutch95@gmail.com>
2017-04-23 23:03:54 -0700

    Started history page

--- simple-calc/simple-calc/ViewController.swift
+++ simple-calc/simple-calc/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     let operands = ["+", "-", "*", "/", "%", "COUNT", "AVG", "FACT"]
     var history = Array<String>()
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
